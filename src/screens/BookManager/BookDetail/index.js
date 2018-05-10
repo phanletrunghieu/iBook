79cdcbf82645375a7b83d25c9a3b75dbcf6ad67b
NguyenKimHieu <nkh170897@gmail.com>
2018-05-10 19:31:30 +0700

    Update BookDetail

--- src/screens/BookManager/BookDetail/index.js
+++ src/screens/BookManager/BookDetail/index.js
@@ -12,6 +12,8 @@ import RaisedButton from 'material-ui/RaisedButton';
 
 import SaveIcon from 'material-ui/svg-icons/content/save';
 
+import {getBookByID} from "../../../api/BookAPI";
+
 
 
 import FlatButton from 'material-ui/FlatButton';
@@ -24,12 +26,21 @@ import config from '../../../config';
 class BookDetailScreen extends Component {
 
   state={
+    BookData: {},
   };
 
   constructor(props){
     super(props);
 
     this.onSelectImage = this.onSelectImage.bind(this);
+    this.loadData=this.loadData.bind(this);
+    this.onChangeName=this.onChangeName.bind(this);
+    this.onChangeImage=this.onChangeImage.bind(this);
+    this.onChangeAuthor=this.onChangeAuthor.bind(this);
+  }
+
+  componentDidMount(){
+    this.loadData();
   }
 
   onSelectImage(e){
@@ -46,8 +57,37 @@ class BookDetailScreen extends Component {
         reader.readAsDataURL(file);
       }
     }
+  }
 
+  onChangeImage(newImg)
+  {
+    var BookData=this.state.BookData;
+    BookData.image=newImg;
+    this.setState({BookData});
+  }
 
+  onChangeName(newName)
+  {
+    var BookData=this.state.BookData;
+    BookData.name=newName;
+    this.setState({BookData});
+  }
+
+  onChangeAuthor(newAuthor)
+  {
+    var BookData=this.state.BookData;
+    //chưa có Author trong BookAPI.
+  }
+
+  loadData()
+  {
+    var BookId=this.props.match.params.bookId;
+    getBookByID(BookId)
+    .then(BookData=>
+      {
+        this.setState({BookData})
+      }
+    )
   }
 
   render() {
@@ -70,28 +110,30 @@ class BookDetailScreen extends Component {
       <div>
         <div style={{overflow:"hidden", padding: "0 50px", marginTop:150}}>
           <div style={{position: "relative", float:"left", width: 200}}>
-            <img className="book-cover" src="https://gacsach.com/sites/gacsach.com/files/styles/book310/public/images/2728/2363e788c756aaa9ff820cc90a6a72c0_0.jpg?itok=O3YxDCgY" alt="Book cover" style={{width:200, height: 300}}  />
+            <img className="book-cover" src={this.state.BookData.image} alt="Book cover" style={{width:200, height: 300}}  />
             <input style={styles.coverInput} type="file" accept="image/*" id="cover-image-upload" name="img" onChange={this.onSelectImage} />
             <label style={styles.coverLabel} htmlFor="cover-image-upload"></label>
 
           </div>
 
-          <div>
-            <div style={{float:"left", marginLeft:50, width: "calc(100% - 250px)"}}>
-                <TextField
+          <div style={{float:"left", marginLeft:50, width: "calc(100% - 250px)"}}>
+            <TextField
+              inputStyle={{fontSize:40, }}
+              hintText="Tên Sách"
+              value={this.state.BookData.name}
+              fullWidth
+              underlineShow={false}
+              onChange={(e,newName)=>this.onChangeName(newName)}
+            />
 
-                inputStyle={{fontSize:40, }}
-                hintText="Tên Sách"
-                defaultValue="Bàn có 5 chỗ ngồi"
-                fullWidth
-                multiLine={true}
-                underlineShow={false}
-                />
-            </div>
+            <TextField
+              inputStyle={{fontSize:20}}
+              floatingLabelFixed={true}
+              floatingLabelText="Tác gỉả"
+            />
 
 
-
-            </div>
+          </div>
 
             <div style={{float:"center", marginLeft:250, marginRight:150, marginTop:50}}>
               <TextField
