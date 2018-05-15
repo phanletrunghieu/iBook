95f0c4a8b15e7e0b27629b7c4508eab7b43b8a0d
NguyenKimHieu <nkh170897@gmail.com>
2018-05-15 09:16:56 +0700

    update Button Save in BookDetail

--- src/screens/BookManager/BookDetail/index.js
+++ src/screens/BookManager/BookDetail/index.js
@@ -12,7 +12,9 @@ import RaisedButton from 'material-ui/RaisedButton';
 
 import SaveIcon from 'material-ui/svg-icons/content/save';
 
-import {getBookByID,editBookName,editBookCover} from "../../../api/BookAPI";
+import Snackbar from 'material-ui/Snackbar';
+
+import {getBookByID,updateBook} from "../../../api/BookAPI";
 
 
 
@@ -27,31 +29,48 @@ class BookDetailScreen extends Component {
 
   state={
     BookData: {},
+    SnackbarMessage:"",
+    deviceWidth: 0,
   };
 
   constructor(props){
     super(props);
 
+    this.updateDimensions = this.updateDimensions.bind(this);
     this.onSelectImage = this.onSelectImage.bind(this);
     this.loadData=this.loadData.bind(this);
     this.onChangeName=this.onChangeName.bind(this);
     this.onChangeImage=this.onChangeImage.bind(this);
     this.onChangeAuthor=this.onChangeAuthor.bind(this);
+    this.onChangeDescription=this.onChangeDescription.bind(this);
     this.save=this.save.bind(this);
   }
 
   componentDidMount(){
+    window.addEventListener("resize", this.updateDimensions);
     this.loadData();
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+  updateDimensions() {
+    this.setState({deviceWidth: window.innerWidth});
   }
 
   onSelectImage(e){
-    console.log(e.target.files);
+    console.log("selected file", e.target.files);
+    var that = this;
     if(e.target.files.length>0){
       var file=e.target.files[0];
       var reader = new FileReader();
 
       reader.onloadend = function () {
         document.querySelector('.book-cover').src = reader.result;
+
+        //lưu hình
+        var BookData=that.state.BookData;
+        BookData.image=reader.result;
+        that.setState({BookData});
       }
 
       if (file) {
@@ -77,8 +96,17 @@ class BookDetailScreen extends Component {
   onChangeAuthor(newAuthor)
   {
     var BookData=this.state.BookData;
-    //chưa có Author trong BookAPI.
+    BookData.author=newAuthor;
+    this.setState({BookData});
   }
+
+  onChangeDescription(newDescription)
+  {
+    var BookData=this.state.BookData;
+    BookData.description=newDescription;
+    this.setState({BookData});
+  }
+
 
   loadData()
   {
@@ -93,11 +121,36 @@ class BookDetailScreen extends Component {
 
   save()
   {
+    var new_data=this.state.BookData;
+    var BookID=new_data.id;
+    updateBook(BookID,new_data)
+    .then(()=>{
+      this.setState({
+        SnackbarMessage:"Đã Lưu"
+      })
+    })
 
   }
 
   render() {
+    var is_desktop = this.state.deviceWidth >= 992;
+
     var styles={
+      container: {
+        overflow:"hidden",
+        padding: is_desktop ? "150px 50px 0" : "10px 50px 0",
+      },
+      coverContainer: {
+        position: "relative",
+        float: is_desktop ? "left" : "none",
+        margin: "0 auto 10px",
+        width: 200
+      },
+      detailContainer: {
+        float: is_desktop ? "left" : "none",
+        marginLeft: is_desktop ? 50 : 0,
+        width: is_desktop ? "calc(100% - 250px)" : "100%"
+      },
       coverInput: {
         display: 'none'
       },
@@ -107,35 +160,31 @@ class BookDetailScreen extends Component {
         left: 0,
         width: "100%",
         height: "100%"
-      }
-    }
-
-    var btSave={
+      },
       floatingActionButton: {
         position: 'fixed',
         bottom: 15,
         right: 15,
       },
-    };
-
+    }
 
     return (
       <div>
-        <div style={{overflow:"hidden", padding: "0 50px", marginTop:150}}>
-          <div style={{position: "relative", float:"left", width: 200}}>
+        <div style={styles.container}>
+          <div style={styles.coverContainer}>
             <img className="book-cover" src={this.state.BookData.image} alt="Book cover" style={{width:200, height: 300}}  />
             <input style={styles.coverInput} type="file" accept="image/*" id="cover-image-upload" name="img" onChange={this.onSelectImage} />
             <label style={styles.coverLabel} htmlFor="cover-image-upload"></label>
 
           </div>
 
-          <div style={{float:"left", marginLeft:50, width: "calc(100% - 250px)"}}>
+          <div style={styles.detailContainer}>
             <TextField
               inputStyle={{fontSize:40, }}
               hintText="Tên Sách"
-              value={this.state.BookData.name}
               fullWidth
               underlineShow={false}
+              value={this.state.BookData.name}
               onChange={(e,newName)=>this.onChangeName(newName)}
             />
 
@@ -143,42 +192,49 @@ class BookDetailScreen extends Component {
               inputStyle={{fontSize:20}}
               floatingLabelFixed={true}
               floatingLabelText="Tác gỉả"
+              value={this.state.BookData.author}
+              onChange={(e,newAuthor)=>this.onChangeAuthor(newAuthor)}
             />
 
-
+            <TextField
+              inputStyle={{fontSize:20}}
+              floatingLabelFixed={true}
+              floatingLabelText="Mô tả nội dung "
+              multiLine={true}
+              fullWidth
+              value={this.state.BookData.description}
+              onChange={(e,newDescription)=>this.onChangeDescription(newDescription)}
+            />
           </div>
-
-            <div style={{float:"center", marginLeft:250, marginRight:150, marginTop:50}}>
-              <TextField
-                inputStyle={{fontSize:20}}
-                floatingLabelFixed={true}
-                floatingLabelText="Mô tả nội dung "
-                multiLine={true}
-                fullWidth
-              />
-          </div>
-
         </div>
 
         <div style={{float:"right"}}>
           {
             this.state.deviceWidth >= 992 ?
             <RaisedButton
-            label="Save"
-            primary={true}
-            icon={<SaveIcon />}
-            btSave={styles.floatingActionButton}
-            onClick={this.save}
+              label="Save"
+              primary={true}
+              icon={<SaveIcon />}
+              style={styles.floatingActionButton}
+              onClick={this.save}
             />
             :
             <FloatingActionButton
-              btSave={styles.floatingActionButton}
+              style={styles.floatingActionButton}
               onClick={this.save}
               >
               <SaveIcon/>
               </FloatingActionButton>
             }
         </div>
+
+        <Snackbar
+          bodyStyle={{textAlign:"center"}}
+          open={this.state.SnackbarMessage!==""}
+          message={this.state.SnackbarMessage}
+          autoHideDuration={4000}
+          onRequestClose={()=>this.setState({SnackbarMessage:""})}
+        />
 
       </div>
     );
